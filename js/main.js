b5f54a31e07fb00ab43b8343c41ae7422fb68497
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-08 01:37:14 +0900

    addname,loadtodo
    바닐라자바스크립ㄴ트 연습연습

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 function login_ev() {
   var login = document.getElementById("login-signup"),
     logWindow = document.getElementById("login"),
@@ -14,39 +15,42 @@ function login_ev() {
 }
 login_ev();
 
-var slideContainer = document.getElementsByClassName("TREDING-POSTS");
-var slidewin = document.getElementsByClassName("slide_box");
-var slideRight = document.getElementById("POST_right");
-var slideLeft = document.getElementById("POST_left");
-var currentIndex = 0;
+function TredingPostsSlide() {
+  var slideContainer = document.getElementsByClassName("TREDING-POSTS");
+  var slidewin = document.getElementsByClassName("slide_box");
+  var slideRight = document.getElementById("POST_right");
+  var slideLeft = document.getElementById("POST_left");
+  var currentIndex = 0;
 
-function goToSlide(idx) {
-  slideContainer[0].style.left = idx * -100 + "%";
-  currentIndex = idx;
-  updateBtt();
-}
-
-slideRight.addEventListener("click", function () {
-  goToSlide(currentIndex + 1);
-});
-slideLeft.addEventListener("click", function () {
-  goToSlide(currentIndex - 1);
-});
-
-function updateBtt() {
-  if (currentIndex == slidewin.length - 1) {
-    slideRight.classList.add("disabled");
-  } else {
-    slideRight.classList.remove("disabled");
+  function goToSlide(idx) {
+    slideContainer[0].style.left = idx * -100 + "%";
+    currentIndex = idx;
+    updateBtt();
   }
 
-  if (currentIndex == 0) {
-    slideLeft.classList.add("disabled");
-  } else {
-    slideLeft.classList.remove("disabled");
+  slideRight.addEventListener("click", function () {
+    goToSlide(currentIndex + 1);
+  });
+  slideLeft.addEventListener("click", function () {
+    goToSlide(currentIndex - 1);
+  });
+
+  function updateBtt() {
+    if (currentIndex == slidewin.length - 1) {
+      slideRight.classList.add("disabled");
+    } else {
+      slideRight.classList.remove("disabled");
+    }
+
+    if (currentIndex == 0) {
+      slideLeft.classList.add("disabled");
+    } else {
+      slideLeft.classList.remove("disabled");
+    }
   }
+  goToSlide(0);
 }
-goToSlide(0);
+TredingPostsSlide();
 
 const imgbtt = document.getElementsByClassName("img_nav");
 const imghover = document.getElementById("main_img-nav");
@@ -59,5 +63,132 @@ for (var i = 0; i < imgbtt.length; i++) {
     }
   });
 }
+imgbtt[0].addEventListener("click", function () {
+  document.getElementById("main_img").style.backgroundImage =
+    "url('img/blog-4.jpg')";
+});
+imgbtt[1].addEventListener("click", function () {
+  document.getElementById("main_img").style.backgroundImage =
+    "url('img/blog-5.jpg')";
+});
+imgbtt[2].addEventListener("click", function () {
+  document.getElementById("main_img").style.backgroundImage =
+    "url('img/blog-6.jpg')";
+});
+*/
 
-add();
+function PreviewSlide() {
+  var slideContainer = document.getElementById("TREDING_silde-container");
+  var slidewin = document.getElementsByClassName("TREDING_silde-list");
+  var slideRight = document.getElementById("TREDING_right");
+  var slideLeft = document.getElementById("TREDING_left");
+  var currentIndex = 0;
+
+  function goToSlide(idx) {
+    slideContainer.style.left = idx * -25 + "%";
+    currentIndex = idx;
+    updateBtt();
+  }
+
+  slideRight.addEventListener("click", function () {
+    goToSlide(currentIndex + 1);
+  });
+  slideLeft.addEventListener("click", function () {
+    goToSlide(currentIndex - 1);
+  });
+
+  function updateBtt() {
+    if (currentIndex == slidewin.length - 4) {
+      slideRight.classList.add("disabled");
+    } else {
+      slideRight.classList.remove("disabled");
+    }
+
+    if (currentIndex == 0) {
+      slideLeft.classList.add("disabled");
+    } else {
+      slideLeft.classList.remove("disabled");
+    }
+  }
+  goToSlide(0);
+}
+PreviewSlide();
+
+const form = document.querySelector(".js-form");
+const input = document.querySelector(".formInput");
+const toDoli = document.querySelector(".js-toDoList");
+const todo = "todo";
+
+function askForName() {
+  form.addEventListener("submit", function hendlerSubmit(ev) {
+    ev.preventDefault();
+    const toToName = input.value;
+    addName(toToName);
+  });
+}
+
+const toDos = [];
+
+function saveTodo() {
+  localStorage.setItem(todo, JSON.stringify(toDos));
+}
+
+function addName(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  span.innerText = text;
+  toDoli.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  const toDoObj = {
+    text: text,
+    Id: newId,
+  };
+  toDos.push(toDoObj);
+  saveTodo();
+}
+
+function loadToDo() {
+  const loadedToDos = localStorage.getItem(todo);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    for (var w = 0; w < parsedToDos.length; w++) {
+      addName(parsedToDos[w].text);
+    }
+  }
+}
+function init() {
+  loadToDo();
+  askForName();
+}
+init();
+/*
+function hendlerSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintNaming(currentValue);
+}
+
+function askUserName() {
+  form.classList.add(showingg);
+  form.addEventListener("submit", hendlerSubmit);
+}
+
+function paintNaming(text) {
+  form.classList.remove(showingg);
+  naming.classList.add(showingg);
+  naming.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askUserName();
+  } else {
+    paintNaming(currentUser);
+  }
+}
+*/
