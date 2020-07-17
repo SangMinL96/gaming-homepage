8a3b636cfe9a69b96f12ae41b41268da2134ae24
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-18 01:28:19 +0900

    수정

--- js/slide.js
+++ js/slide.js
@@ -1,73 +1,33 @@
-function TredingPosts() {
-  var slideContainer = document.getElementsByClassName("TREDING-POSTS");
-  var slidewin = document.getElementsByClassName("slide_box");
-  var slideRight = document.getElementById("POST_right");
-  var slideLeft = document.getElementById("POST_left");
-  var currentIndex = 0;
+const slideContainer = document.getElementsByClassName("TREDING-POSTS");
+const slidewin = document.getElementsByClassName("slide_box");
+const slideRight = document.getElementById("POST_right");
+const slideLeft = document.getElementById("POST_left");
+let currentIndex = 0;
 
-  function goToSlide(idx) {
-    slideContainer[0].style.left = idx * -100 + "%";
-    currentIndex = idx;
-    updateBtt();
-  }
-
-  slideRight.addEventListener("click", function () {
-    goToSlide(currentIndex + 1);
-  });
-  slideLeft.addEventListener("click", function () {
-    goToSlide(currentIndex - 1);
-  });
-
-  function updateBtt() {
-    if (currentIndex == slidewin.length - 1) {
-      slideRight.classList.add("disabled");
-    } else {
-      slideRight.classList.remove("disabled");
-    }
-
-    if (currentIndex == 0) {
-      slideLeft.classList.add("disabled");
-    } else {
-      slideLeft.classList.remove("disabled");
-    }
-  }
-  goToSlide(0);
+function goToSlide(idx) {
+  slideContainer[0].style.left = idx * -100 + "%";
+  currentIndex = idx;
+  updateBtt();
 }
-TredingPosts();
 
-function PreviewSlide() {
-  var slideContainer = document.getElementById("TREDING_silde-container");
-  var slidewin = document.getElementsByClassName("TREDING_silde-list");
-  var slideRight = document.getElementById("TREDING_right");
-  var slideLeft = document.getElementById("TREDING_left");
-  var currentIndex = 0;
+slideRight.addEventListener("click", function () {
+  goToSlide(currentIndex + 1);
+});
+slideLeft.addEventListener("click", function () {
+  goToSlide(currentIndex - 1);
+});
 
-  function goToSlide(idx) {
-    slideContainer.style.left = idx * -25 + "%";
-    currentIndex = idx;
-    updateBtt();
+function updateBtt() {
+  if (currentIndex == slidewin.length - 1) {
+    slideRight.classList.add("disabled");
+  } else {
+    slideRight.classList.remove("disabled");
   }
 
-  slideRight.addEventListener("click", function () {
-    goToSlide(currentIndex + 1);
-  });
-  slideLeft.addEventListener("click", function () {
-    goToSlide(currentIndex - 1);
-  });
-
-  function updateBtt() {
-    if (currentIndex == slidewin.length - 4) {
-      slideRight.classList.add("disabled");
-    } else {
-      slideRight.classList.remove("disabled");
-    }
-
-    if (currentIndex == 0) {
-      slideLeft.classList.add("disabled");
-    } else {
-      slideLeft.classList.remove("disabled");
-    }
+  if (currentIndex == 0) {
+    slideLeft.classList.add("disabled");
+  } else {
+    slideLeft.classList.remove("disabled");
   }
-  goToSlide(0);
 }
-PreviewSlide();
+goToSlide(0);
