8d53e4825d0e10d3bd89e4f3c15216da762f450d
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-18 01:28:13 +0900

    preView 버튼슬라이드

---
+++ js/preViewSlide.js
@@ -0,0 +1,21 @@
+const PV_slideContainer = document.getElementById("TREDING_silde-container");
+const PV_nav = document.getElementsByClassName("TREDING_nav-list");
+
+function onChack() {
+  for (let a = 0; a < PV_nav.length; a++) {
+    PV_nav[a].addEventListener("click", (ev) => {
+      for (let z = 0; z < PV_nav.length; z++) {
+        PV_nav[z].classList.remove("active");
+        ev.target.classList.add("active");
+        goToChack(ev.target.id);
+      }
+    });
+  }
+}
+function goToChack(id) {
+  PV_slideContainer.style.left = id * -25 + "%";
+}
+function init() {
+  onChack();
+}
+init();
